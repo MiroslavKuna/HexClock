81060fd4617e98ac6d67cd75d54d226db6a7d46b
MiroslavKuna <kuna@spsejecna.cz>
2022-12-16 19:10:10 +0100

    Done 🤓✅

---
+++ main.js
@@ -0,0 +1,30 @@
+setInterval(updateBg, 1000);
+setInterval(updateText, 1000);
+
+function getTimeAsHex(){
+    var date = new Date();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+
+    if(hour < 10) { hour = "0" + hour; }
+    if(minute < 10)  { minute = "0" + minute; }
+    if(second < 10)  { second = "0" + second; }
+
+    return "#" + hour + minute + second;
+}
+
+function updateBg(){
+    const bod = document.getElementsByTagName("body")[0];
+    bod.style.backgroundColor = getTimeAsHex();
+}
+
+function updateText(){
+    const time = document.getElementsByClassName("clock")[0];
+    time.innerHTML = getTimeAsHex();
+}
+
+function init(){
+    setInterval(updateBg, 1000);
+    setInterval(updateText, 1000);
+}
